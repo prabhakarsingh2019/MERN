28feb74f1ca8dcade40d4c805110a0bed47e37e3
Prabhakar Singh <prabhakarsingh8722@gmail.com>
2024-11-21 10:54:57 +0530

    fixed cookie

--- quiz-app/backend/controllers/userAuthController.js
+++ quiz-app/backend/controllers/userAuthController.js
@@ -35,6 +35,7 @@ export const signup = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -78,6 +79,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
